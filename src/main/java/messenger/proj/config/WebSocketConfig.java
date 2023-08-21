120a2f76fd378fd47d2e3c8404d8ab26a96ecb3d
aeaeaeaeaeaeaeaea <aleseysenvkov@mail.ru>
2023-08-21 19:11:04 +0300

    123

--- src/main/java/messenger/proj/config/WebSocketConfig.java
+++ src/main/java/messenger/proj/config/WebSocketConfig.java
@@ -5,6 +5,8 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import messenger.proj.webSocket.ChatWebSocketHandler;
+
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
--- src/main/java/messenger/proj/models/message.java
+++ src/main/java/messenger/proj/models/message.java
@@ -1,7 +1,5 @@
 package messenger.proj.models;
 
-import java.util.UUID;
-
 import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
@@ -10,34 +8,125 @@ import org.springframework.data.cassandra.core.mapping.Table;
 public class message {
 	
 	@PrimaryKey
-	private UUID id;
+	private String id;
 	
-	@Column("message")
-	private String message;
+	@Column("chatId")
+	private String chatId;
 	
-	public message() {
+	@Column("senderId")
+	private String senderId;
+	
+	@Column("recipientId")
+	private String recipientId;
+	
+	@Column("senderName")
+	private String senderName;
+	
+	@Column("recipientName")
+	private String recipientName;
+	
+	@Column("content")
+	private String content;
+	
+	@Column("status")
+	private String status;
+
+	
+	public message() {	
+	}
+
+
+	public message(String id, String chatId, String senderId, String recipientId, String senderName,
+			String recipientName, String content, String status) {
+		super();
+		this.id = id;
+		this.chatId = chatId;
+		this.senderId = senderId;
+		this.recipientId = recipientId;
+		this.senderName = senderName;
+		this.recipientName = recipientName;
+		this.content = content;
+		this.status = status;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getChatId() {
+		return chatId;
+	}
+
+
+	public void setChatId(String chatId) {
+		this.chatId = chatId;
+	}
+
+
+	public String getSenderId() {
+		return senderId;
+	}
+
+
+	public void setSenderId(String senderId) {
+		this.senderId = senderId;
+	}
+
+
+	public String getRecipientId() {
+		return recipientId;
+	}
+
+
+	public void setRecipientId(String recipientId) {
+		this.recipientId = recipientId;
+	}
+
+
+	public String getSenderName() {
+		return senderName;
+	}
+
+
+	public void setSenderName(String senderName) {
+		this.senderName = senderName;
+	}
+
+
+	public String getRecipientName() {
+		return recipientName;
+	}
+
+
+	public void setRecipientName(String recipientName) {
+		this.recipientName = recipientName;
+	}
+
+
+	public String getContent() {
+		return content;
+	}
+
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
 		
-	}
-	
-	public message(UUID id, String message) {
-		this.id = id;
-		this.message = message;
-	}
-	
-	public void setId(UUID id) {
-		this.id = id;
-	}
-	
-	public UUID getId() {
-		return this.id;
-	}
-	
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	
-	public String getMessage() {
-		return this.message;
-	}
-	
 }
