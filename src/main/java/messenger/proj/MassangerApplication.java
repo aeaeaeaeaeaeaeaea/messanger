da87fa47bed60f38d6d75c7578f1b59826d20e52
aeaeaeaeaeaeaeaea <aleseysenvkov@mail.ru>
2023-09-25 20:50:17 +0300

    1

--- src/main/java/messenger/proj/MassangerApplication.java
+++ src/main/java/messenger/proj/MassangerApplication.java
@@ -1,7 +1,15 @@
 package messenger.proj;
 
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.settings.Settings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
 
 @SpringBootApplication
 public class MassangerApplication {
@@ -9,5 +17,6 @@ public class MassangerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MassangerApplication.class, args);
 	}
+
 
 }
---
+++ src/main/java/messenger/proj/config/ElasticSearchConfig.java
@@ -0,0 +1,17 @@
+package messenger.proj.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticSearchConfig {
+
+	@Bean
+	public RestHighLevelClient elasticSearchClient() {
+		return new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+	}
+}
